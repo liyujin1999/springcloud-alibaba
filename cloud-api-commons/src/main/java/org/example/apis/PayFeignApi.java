62caca7b4faefe7ff7b24e6a8b9a04be5551267a
yujin <1984626579@qq.com>
2024-11-22 11:04:32 +0800

    引入Resilience4J作为熔断、降级、限流

--- cloud-api-commons/src/main/java/org/example/apis/PayFeignApi.java
+++ cloud-api-commons/src/main/java/org/example/apis/PayFeignApi.java
@@ -21,4 +21,23 @@ public interface PayFeignApi {
 
     @GetMapping("/pay/get/info")
     public String mylb();
+
+    @GetMapping("/pay/circuit/{id}")
+    public ResultData circuit(@PathVariable("id") Integer id);
+
+    /**
+     * Resilience4j Bulkhead 的例子
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/pay/bulkhead/{id}")
+    public String myBulkhead(@PathVariable("id") Integer id);
+
+    /**
+     * Resilience4j Ratelimit 的例子
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/pay/ratelimit/{id}")
+    public String myRatelimit(@PathVariable("id") Integer id);
 }
